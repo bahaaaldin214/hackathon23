fad9dbc2f6b37dbe3a5c0088bcd07ede3a1660a0
Bahaa <bahaaaldinmohammad@gmail.com>
2023-09-24 03:24:48 -0500

    offically able to select mouscle groups

--- static/main.js
+++ static/main.js
@@ -20,8 +20,8 @@ function makeSpan(text, className){
 
     span.textContent =  `${text}`; //can customize furthor
 
-    span.class = className;
-
+    span.classList.add(className);
+    console.log(span.class)
     return span;
 }
 
@@ -30,28 +30,51 @@ window.onload = function(){
     const [askAI, textBox, chatinput, canvas, buffer] = selector("form", "#chatoutput", "#chatinput", "#canvas", "#buffer"),
     colors = {
         front: {
-            "0,255,85": "Fore Arm ",
-            "255,0,29": "Shoulder",
-            "42,0,55": "Chest",
-            "0,255,255": "Biceps",
-            "255,255,0": "Abs",
-            "255,0,170": "Quads"
+            "0,255,85": "forearms",
+            "255,0,29": "shoulders",
+            "42,0,255": "chest",
+            "0,255,255": "biceps",
+            "255,255,0": "abs",
+            "255,0,170": "quads"
         },
         back: {
-            "255,0,16": "Triceps",
-            "170,255,0": "Lats",
-            "186,0,255": "Glutes",
-            "255,0,216": "Calvas"
+            "255,0,16": "triceps",
+            "170,255,0": "lats",
+            "186,0,255": "glutes",
+            "255,0,216": "calves"
         }
     };
 
-    postData("/updateUser", {id: "myUser", password: "123Hello"}).then((data) => {
+    postData("/updateUser", {id: "myUser", password: "123Hello"}).then(async (data) => {
     
         //use colors to make muscle group object with informations on it
-        const user = new User(id, data, colors),
-        display = new Display(canvas, buffer, colors);
+        const user = new User(id, data, colors);
+        
 
-        display.image("/assets/visual.png", "/assets/presentable.png")
+        // fetch("/static/assets/visual.png")
+        // .then(src => {
+        //     const image = new Image();
+        //     // image.src = src;
+        // })
+        // .catch(error => {
+        //     // Handle any errors that occurred during the fetch
+        //     console.error("Fetch error:", error);
+        // });
+        // const presentable = fetch("/static/assets/presentable.png")
+        // .catch(error => {
+        //     // Handle any errors that occurred during the fetch
+        //     console.error("Fetch error:", error);
+        // });
+
+        const visual = new Image();
+        visual.src  = "/static/assets/visual.png";
+
+        const presentable = new Image();
+        presentable.src  = "/static/assets/presentable.png";
+
+        const display = new Display(canvas, buffer, colors, visual, presentable);
+        
+        window.addEventListener("mousemove", (e) => display.getPixel(e))
 
     });
     
@@ -79,11 +102,11 @@ window.onload = function(){
     
     }
 
-    // askAI.onsubmit = function(e){
-    //     if(!aiBusy){
-    //         pushPrompt(e)
-    //     }
-    // }
+    askAI.onsubmit = function(e){
+        if(!aiBusy){
+            pushPrompt(e)
+        }
+    }
 
     // askAI.addEventListener("submit", function(e){
     //     console.log(textBox)
--- static/modules/display.js
+++ static/modules/display.js
@@ -1,19 +1,56 @@
 export default class Display {
-    constructor(canvas, buffer, colors) {
+    constructor(canvas, bufferCanvas, colors, color, blank) {
         this.context = canvas.getContext("2d");
-        this.buffer = buffer.getContext("2d");
+        this.buffer = bufferCanvas.getContext("2d");
+
+
+        this.blank = blank; 
 
         this.faceFront = true;
 
         this.colors = colors
+        
+        for (let color in colors.front) {
+            let src = "/static/assets/"+colors.front[color]+".png";
+            let image = new Image()
+            image.src = src;
+            colors.front[color] = image;
+
+        }
+        
+        for (let color in colors.back) {
+            let src = "/static/assets/"+colors.back[color]+".png";
+            let image = new Image()
+            image.src = src;
+            colors.back[color] = image;
+        }
+
+        let images = 0;
+         let handleImage = () => {
+            images++
+            if(images == 2){
+                bufferCanvas.height 
+                bufferCanvas.width  = canvas.width = 2008;
+                bufferCanvas.height  = canvas.height = 1298;
+                
+                bufferCanvas.width 
+                this.image(this.context, blank)
+                this.image(this.buffer, color)
+                
+            }
+        }
+        blank.onload = handleImage;
+        color.onload = handleImage;
+        
     }
-    image(color, blank) {
-        const { context, buffer } = this;
+    image(ctx, img) {
 
-        const scalar = .8
+        const scalar = .2
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        context.drawImage(blank, 0, 0, 490, 649, 0, 0, 490 * scalar, 649 * scalar);
-        buffer.drawImage(color, 0, 0, 490, 649, 0, 0, 490 * scalar, 649 * scalar);
+
+        ctx.drawImage(img, 0, 0, img.width/2, img.height, 0, 0, img.width , img.height );
+        
         //490 for end of left
     }
 
@@ -21,7 +58,29 @@ export default class Display {
         // this.conext
     }
 
-    getPixel(x, y) {
-        return "limb"
+    getPixel(event) {
+        const {buffer, context, context: {canvas}} = this;
+        const viewWidth = canvas.getBoundingClientRect().right - canvas.getBoundingClientRect().left;
+        const vewHeigh = canvas.getBoundingClientRect().bottom - canvas.getBoundingClientRect().top;
+
+        const mouseX = (event.clientX - canvas.getBoundingClientRect().left)/viewWidth*canvas.width;
+        const mouseY = (event.clientY - canvas.getBoundingClientRect().top)/vewHeigh*canvas.height;
+    
+        // Get the color of the pixel at the clicked coordinates
+        const pixelData = buffer.getImageData(mouseX, mouseY, 1, 1).data;
+    
+        // Extract the RGBA values from the pixel data
+        const red = pixelData[0];
+        const green = pixelData[1];
+        const blue = pixelData[2];
+        const str = `${red},${green},${blue}`
+
+        if(this.colors.front[str]){
+            
+            this.image(context, this.colors.front[str])
+            
+        } else {
+            this.image(context, this.blank)
+        }
     }
 }
